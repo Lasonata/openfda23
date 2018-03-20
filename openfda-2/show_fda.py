e87c89e9c953c2e4261f92982aaa1b1bb3d31cf7
lasonata <lasomielgopablo@gmail.com>
2018-03-20 16:53:53 +0100

    upload #1

--- openfda-2/show_fda.py
+++ openfda-2/show_fda.py
@@ -15,4 +15,4 @@ repos = json.loads(repos_raw)
 
 print("The id is:", repos['results'][0]["id"]) # enter dictionary {} inside another dictionary
 print("The purpose is:", repos['results'][0]["purpose"])
-print("The purpose is:", repos['results'][0]["openfda"]["manufacturer_name"])
+print("The manufacturer name is:", repos['results'][0]["openfda"]["manufacturer_name"])
