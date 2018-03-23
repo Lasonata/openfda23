cd7abb13ae623792fc731f42b2e97c730f66b3ca
lasonata <lasomielgopablo@gmail.com>
2018-03-23 15:58:40 +0100

    upload #1 (working)

--- openfda-1/show_fda(1).py
+++ openfda-1/show_fda(1).py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?limit=10", None, headers)
+conn.request("GET", "/drug/label.json?limit=1", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
@@ -12,7 +12,6 @@ conn.close()
 
 repos = json.loads(repos_raw)
 
-
 print("The id is:", repos['results'][0]["id"])
-print("The purpose is:", repos['results'][0]["purpose"])
-print("The manufacturer name is:", repos['results'][0]["openfda"]["manufacturer_name"])
+print("The purpose is.", repos['results'][0]["purpose"])
+print("The purpose is:", repos['results'][0]["openfda"]["manufacturer_name"])
